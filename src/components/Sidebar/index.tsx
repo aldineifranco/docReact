79fbcfae032b876f5e12f295979fd7af4e41450f
aldinei <aldineifranco@gmail.com>
2024-06-03 22:11:58 -0300

    add button link sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
-import { Container } from "./style";
+import { Container, LinkSidebar } from "./style";
 import { Button } from "../Button";
 
 export function Sidebar() {
   return (
     <Container>
-      <Button title="Lista de Vídeos" variant="primary" />
-      <Link to="/">lista de Vídeos</Link>
-      <Link to="/view">Detalhe dos Vídeos</Link>
+      <LinkSidebar to="/">Lista de Vídeos</LinkSidebar>
+      <LinkSidebar to="/view">Detalhe dos Vídeos</LinkSidebar>
     </Container>
   )
 }
--- src/components/Sidebar/style.ts
+++ src/components/Sidebar/style.ts
@@ -1,12 +1,27 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Container = styled.div`
   height: calc(100vh - 120px);
   grid-area: sidebar;
   background-color: #fff;
+  border-radius: 8px;
   width: 240px;
   display: flex;
   flex-direction: column;
   align-items: start;
-  gap: 16px
+  gap: 8px;
+  padding: 12px;
 `;
+
+export const LinkSidebar = styled(Link)`
+background-color: ${({ theme }) =>
+    theme.colors.primary};
+  color: ${({ theme }) =>
+    theme.colors.white};
+  padding: 16px;
+  border-radius: 8px;
+  border: 0;
+  width: 100%;
+  text-align: center;
+`
